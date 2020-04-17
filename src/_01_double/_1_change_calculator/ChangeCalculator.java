10d0cbf8b9145fd0c986c6816d959a02fa47381e
root <root@70236f44e438>
2020-04-17 18:25:45 +0000

    Test Scores

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,17 +12,19 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String nickels = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+		int nic = Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+		int dim = Integer.parseInt(dimes);
 		// Ask the user how many quarters they have, and convert their answer
-
+		String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+		int quart = Integer.parseInt(quarters);
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+		double money = 0.05*nic+0.1*dim+0.25*quart;
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
-
+		JOptionPane.showMessageDialog(null, "You have "+money+" dollars");
 	}
 }
 
---
+++ src/_01_double/_2_test_scores/Test_score.java
@@ -0,0 +1,12 @@
+package _01_double._2_test_scores;
+
+import javax.swing.JOptionPane;
+
+public class Test_score {
+	public static void main(String[] args) {
+		String score = JOptionPane.showInputDialog("What is your test score?");
+		double test = Integer.parseInt(score);
+		
+		
+	}
+}
